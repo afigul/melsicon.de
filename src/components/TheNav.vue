7bf925d43fd5b1f337f7360675d0d75d841bcc03
vicbergquist <victoria.bergquist@gmail.com>
2018-07-12 15:05:32 +0200

    added nav links (temp)

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,21 +1,26 @@
 <template>
     <nav class="navigation">
-        <a href="#" class="nav-link">Nav</a>
+        <a href="#" class="nav-link">Home</a>
+        <a href="#" class="nav-link">About</a>
+        <a href="#" class="nav-link">Services</a>
+        <a href="#" class="nav-link">Community</a>
+        <a href="#" class="nav-link">Team</a>
+        <a href="#" class="nav-link">Contact</a>
     </nav>
 </template>
 
 <script>
 export default {
-    name: 'Navigation'
+  name: 'Navigation'
 }
 </script>
 
 <style lang="sass" scoped>
 
-.nav
-	position: sticky
-	top: 0
-	height: 3rem
-	background: white
+  .navigation
+    position: sticky
+    top: 0
+    height: 3rem
+    background: white
 
 </style>
